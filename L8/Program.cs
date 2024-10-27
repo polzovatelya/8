c4bd0576b434bc6decfadccca36d607bd352eafb
Mariya <gagagagaga54@mail.ru>
2024-10-27 23:02:02 +0300

    version3_Input

--- L8/Program.cs
+++ L8/Program.cs
@@ -23,6 +23,32 @@ namespace L8
             }
 
         }
+        public List<elem> Input(int count)
+        {   List<elem> lst = new List<elem>();
+            if (count != 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    elem el = new elem();
+                    Console.WriteLine("Введите название команды: ");
+                    el.name_k = Console.ReadLine();
+                    Console.WriteLine("Введите ФИО тренера: ");
+                    el.name_t = Console.ReadLine();
+                    Console.WriteLine("Введите число побед: ");
+                    el.win = Convert.ToInt32(Console.ReadLine());
+                    lst.Add(el);
+
+                }
+            }
+            else
+            {
+                elem el = new elem();
+                el.name_k = "Error";
+                el.name_t = "Error";
+                el.win = -1;
+            }
+            return lst;
+        }
         static void Main(string[] args)
         {
             List<elem> list = new List<elem>();
